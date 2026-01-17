12891409a4fd645eeb934d5018fd534cd5f25ee1
bogdan parkhomchuk <parhomcukbogdan81@gmail.com>
2026-01-17 14:11:59 +0200

    HW_LESSON_4.py

--- HW_lesson_4.py
+++ HW_lesson_4.py
@@ -11,16 +11,19 @@ edges = cv2.Canny(blurred, 50, 150)
 
 contours, _= cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+objects_found = 0
+
 for contour in contours:
     area = cv2.contourArea(contour)
     
     if area > 100:        
         x, y, w, h = cv2.boundingRect(contour)
+        objects_found += 1
         
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 3)
 
+print(f'Кількість знайдених об\'єктів: {objects_found}')
 cv2.imshow('img', img)
-
 cv2.imwrite('rendered_images/fridge_detected.jpg', img)
 
 cv2.waitKey(0)
