7e9fe170e8a41941e15e3390b0d48d702f2a450a
God Given (GG) <parhomcukbogdan81@gmail.com>
2026-03-03 15:17:40 +0200

    Add files via upload

--- CW_lesson_8.py
+++ CW_lesson_8.py
@@ -1,71 +1,78 @@
 import cv2
 import numpy as np
 
-frame = cv2.imread('images/candies.jpg')
-frame = cv2.resize(frame, (800, 600))
-frame = cv2.flip(frame, 1)
-hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+face_cascade = cv2.CascadeClassifier('data/haarcascade/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('data/haarcascade/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('data/haarcascade/haarcascade_smile.xml')
 
-lower_red_1 = np.array([0, 100, 100])
-upper_red_1 = np.array([10, 255, 255])
-lower_red_2 = np.array([160, 100, 100])
-upper_red_2 = np.array([180, 255, 255])
+face_net = cv2.dnn.readNetFromCaffe('data/dnn/deploy.prototxt', 'data/dnn/res10_300x300_ssd_iter_140000.caffemodel')
 
-lower_blue_1 = np.array([100, 100, 100])
-upper_blue_1 = np.array([130, 255, 255])
+cap = cv2.VideoCapture(0)
 
-lower_yellow_1 = np.array([20, 100, 100])
-upper_yellow_1 = np.array([30, 255, 255])
+while True:
+    ret, frame = cap.read()
+    if not ret:
 
-mask_red_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
-mask_red_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
-mask_red = cv2.bitwise_or(mask_red_1, mask_red_2)
+        break
 
-mask_blue = cv2.inRange(hsv, lower_blue_1, upper_blue_1)
+#____________________DNN____________________
+    (h, w) = frame.shape[:2]
 
-mask_yellow = cv2.inRange(hsv, lower_yellow_1, upper_yellow_1)
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0)) #адаптування зображення під нейронку
 
-contours_red, _ = cv2.findContours(mask_red, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours_blue, _ = cv2.findContours(mask_blue, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours_yellow, _ = cv2.findContours(mask_yellow, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    face_net.setInput(blob) #пропускання зображення через нейронку
+    detections = face_net.forward()
 
-#мені здається тут краще було б зробити через функцію, щоб не дублювати те саме три рази, але я не дуже розумію як саме, тому вже як є
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
 
-for cnt in contours_red:
-    area = cv2.contourArea(cnt)
-    if area > 1500:
-        cv2.drawContours(frame, [cnt], -1, (0, 0, 255), 2)
-        mnt = cv2.moments(cnt)
-        if mnt['m00'] != 0:
-            cx = int(mnt['m10'] / mnt['m00'])
-            cy = int(mnt['m01'] / mnt['m00'])
-            cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-            cv2.putText(frame, 'Red', (cx-50, cy-50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 
-for cnt in contours_blue:
-    area = cv2.contourArea(cnt)
-    if area > 1500:
-        cv2.drawContours(frame, [cnt], -1, (255, 0, 0), 2)
-        mnt = cv2.moments(cnt)
-        if mnt['m00'] != 0:
-            cx = int(mnt['m10'] / mnt['m00'])
-            cy = int(mnt['m01'] / mnt['m00'])
-            cv2.circle(frame, (cx, cy), 5, (255, 0, 0), -1)
-            cv2.putText(frame, 'Blue', (cx-10, cy-50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
-            
-for cnt in contours_yellow:
-    area = cv2.contourArea(cnt)
-    if area > 1500:
-        cv2.drawContours(frame, [cnt], -1, (0, 255, 255), 2)
-        mnt = cv2.moments(cnt)
-        if mnt['m00'] != 0:
-            cx = int(mnt['m10'] / mnt['m00'])
-            cy = int(mnt['m01'] / mnt['m00'])
-            cv2.circle(frame, (cx, cy), 5, (0, 255, 255), -1)
-            cv2.putText(frame, 'Yellow', (cx-40, cy-50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
-            
-cv2.imwrite("rendered_images/candies_detected.jpg", frame)
-cv2.imshow('frame', frame)
+            (x1, y1, x2, y2) = box.astype('int')
 
-cv2.waitKey(0)
+            x1, y1 = max(0, x1), max(0, y1)
+            x2, y2 = min(w-1, x2), min(h-1, y2)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imshow('dnn', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
+
+#____________________Cascades____________________
+#     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#
+#     faces = face_cascade.detectMultiScale(grey, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)) #детекутємо обличчя
+#
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) #малюємо прямокуктнк навколо обличчя
+#
+#         roi_gray = grey[y:y + h, x:x + w]
+#         roi_color = frame[y:y + h, x:x + w]
+#
+#         eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=10, minSize=(30, 30)) #детектуємо очі
+#
+#         smile = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=10, minSize=(15, 15)) #детектуємо очі
+#
+#         for (ex, ey, ew, eh) in eyes:
+#             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2) #малюємо прямокуктнк навколо очей
+#
+#         for (ix, iy, iw, ih) in smile:
+#             cv2.rectangle(roi_color, (ix, iy), (ix + iw, iy + ih), (0, 0, 255), 2)  # малюємо прямокуктнк навколо посмішки
+#
+#     cv2.putText(
+#         frame, f'Faces detected: {len(faces)}.',
+#         (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1
+#     )
+#
+#     cv2.imshow('Haar face tracking', frame)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cap.release()
+# cv2.destroyAllWindows()
